5ad07455b0ce2ae060f848e08d789816c5c5d6f7
b1narystar <zfout@smu.edu>
2015-04-14 08:06:29 -0500

    completed hw5

--- Homework/hw5/orbital_fit.m
+++ Homework/hw5/orbital_fit.m
@@ -10,17 +10,18 @@
 %   t       sampled sidereal period values
 %   x       sampled distance values
 %
-function y = orbital_fit(t, x)
+function y = orbital_fit(x, tNodes, dNodes)
   numer_sum = 0.0;
   denom_sum = 0.0;
   c = 0.0;
-  N = length(t);
+  N = length(tNodes);
   for k=1:N
-      numer_sum = numer_sum + ((x(k) .^ 1.5) * t(k)); % calculate numer
-      denom_sum = denom_sum + (x(k) .^ 3); % calculate denom
+      numer_sum = numer_sum + ((dNodes(k) .^ 1.5) * tNodes(k)); % calculate numer
+      denom_sum = denom_sum + (dNodes(k) .^ 3); % calculate denom
   end
   c = numer_sum ./ denom_sum; % find the constant
-  for k=1:N
+  M = length(x);
+  for k=1:M
       y(k) = c * (x(k) .^ 1.5); % find the function values
   end
 end
